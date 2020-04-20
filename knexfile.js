ca6b5a95304807cfaeebbd345e4000c4028a5fbd
Kingmercy-Rayne <kingrayne66@gmail.com>
2020-04-20 22:05:31 +0800

    Add production connection for deploy

--- knexfile.js
+++ knexfile.js
@@ -5,4 +5,8 @@ module.exports = {
     client: "pg",
     connection: "postgres://postgres:123456@localhost:5432/fs-forum",
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+  },
 };
